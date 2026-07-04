8036be9c35fb59a43671d199e2cab9c1d0d3817a
Jacob Anderson <jacobdanderson@gmail.com>
2026-07-04 15:16:14 -0400

    Apply coding standard to Pythonic patterns solutions

--- PDP-11-pdp6-decorator-proxy-and-facade-supplemental-2/solution/main.py
+++ PDP-11-pdp6-decorator-proxy-and-facade-supplemental-2/solution/main.py
@@ -1,15 +1,39 @@
+#################
+#   CONSTANTS   #
+#################
+
+SAMPLE_TEXT = "design patterns"
+
+
+#############
+#   TYPES   #
+#############
+
+# Define the formatting strategy interface
 class Formatter:
+	"""Define the formatter interface"""
+
 	def format(self, text: str) -> str:
+		"""Format the provided text"""
 		raise NotImplementedError
 
 
+# Format text by capitalizing the first letter of each word
 class TitleFormatter(Formatter):
+	"""Format text in title case"""
+
 	def format(self, text: str) -> str:
+		"""Convert text to title case"""
 		return text.title()
 
 
+#################
+#   FUNCTIONS   #
+#################
+
 def main() -> None:
-	print(TitleFormatter().format("design patterns"))
+	"""Print a title-formatted sample phrase"""
+	print(TitleFormatter().format(SAMPLE_TEXT))
 
 
 if __name__ == "__main__":
